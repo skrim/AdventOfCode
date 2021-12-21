c181805029f2322f1cc8bbb983685091b9aa96a5
skrim <matias@arje.net>
2021-12-21 18:32:30 +0000

    Add day 21

--- 2021/21/Main.purs
+++ 2021/21/Main.purs
@@ -1,79 +1,19 @@
-module Main
-  ( main )
-  where
+module Main ( main ) where
 
-import Data.String.Pattern
-import Data.Traversable
-import Math hiding (min)
+import Data.Traversable (sum)
+import Math ((%))
 import Prelude
 
-import Control.Monad.State (state)
-import Data.Array (concat, difference, filter, head, length, null, tail)
-import Data.Int (fromString, fromNumber, toNumber)
-import Data.Maybe (Maybe)
-import Data.Maybe (fromMaybe, maybe)
-import Data.String (split)
-import Data.String.NonEmpty (class MakeNonEmpty)
+import Data.Array (filter)
+import Data.Int (fromNumber, toNumber)
+import Data.Maybe (fromMaybe)
 import Debug (trace)
 import Effect (Effect)
-import Effect.Class (liftEffect)
-import Effect.Console (log, logShow)
-import Node.Encoding (Encoding(..))
-import Node.FS.Sync (readTextFile, readdir)
+import Effect.Console (log)
 
 type Player = { position :: Int, score :: Int, wins :: Int }
-type GameState = { prev :: Player,  next :: Player, die :: Int, rollCount :: Int }
+type GameState = { prev :: Player,  next :: Player, die :: Int, rollCount :: Int, multiplier :: Int }
 type Turn = { die :: Int, delta :: Int }
-type DieAggregate = { result :: Int, count :: Int }
-
-aggregates :: Array DieAggregate
-aggregates = [
-  { result: 1, count: 1 },
-  { result: 1, count: 1 },
-  { result: 2, count: 1 },
-  { result: 2, count: 1 }
-]
-{-
-aggregates = [
-  { result: 3, count: 1 },
-  { result: 4, count: 3 },
-  { result: 5, count: 6 },
-  { result: 6, count: 7 },
-  { result: 7, count: 6 },
-  { result: 8, count: 3 },
-  { result: 9, count: 1 }
-]
-
-aggregates = [
-  { result: 3, count: 1 },
-  { result: 4, count: 1 },
-  { result: 5, count: 1 },
-  { result: 4, count: 1 },
-  { result: 5, count: 1 },
-  { result: 6, count: 1 },
-  { result: 5, count: 1 },
-  { result: 6, count: 1 },
-  { result: 7, count: 1 },
-  { result: 4, count: 1 },
-  { result: 5, count: 1 },
-  { result: 6, count: 1 },
-  { result: 5, count: 1 },
-  { result: 6, count: 1 },
-  { result: 7, count: 1 },
-  { result: 6, count: 1 },
-  { result: 7, count: 1 },
-  { result: 8, count: 1 },
-  { result: 5, count: 1 },
-  { result: 6, count: 1 },
-  { result: 7, count: 1 },
-  { result: 6, count: 1 },
-  { result: 7, count: 1 },
-  { result: 8, count: 1 },
-  { result: 7, count: 1 },
-  { result: 8, count: 1 },
-  { result: 9, count: 1 }
-]
--}
 
 -- part 1
 
@@ -99,7 +39,8 @@ next state = do
       score: state.next.score + newPos,
       wins: 0
     },
-    rollCount: state.rollCount + 3
+    rollCount: state.rollCount + 3,
+    multiplier: 1
   }
 
 final :: GameState -> (GameState -> Boolean) -> GameState
@@ -111,47 +52,53 @@ part1Result state = state.rollCount * (min state.next.score state.prev.score)
 
 -- part 2
 
+completionScore :: Int
+completionScore = 8
+
 winScore :: Int -> Int
-winScore score | score >= 21 = 1
+winScore score | score >= completionScore = 1
                | otherwise = 0
 
-next2 :: DieAggregate -> GameState -> GameState
-next2 aggregate state = do
-  let newPos = 1 + (fromMaybe 0 $ fromNumber $ ((toNumber (state.next.position + aggregate.result) - 1.0) % 10.0))
+next2 :: Int -> GameState -> GameState
+next2 delta state = do
+  let newPos = 1 + (fromMaybe 0 $ fromNumber $ ((toNumber (state.next.position + delta) - 1.0) % 10.0))
   let newScore = state.next.score + newPos
   { die: 0,
     next: state.prev,
     prev: {
       position: newPos,
       score: newScore,
-      wins: aggregate.count * winScore newScore
+      wins: winScore newScore
     },
-    rollCount: 0
+    rollCount: 0,
+    multiplier: state.multiplier
   }
 
 part2Completed :: GameState -> Boolean
-part2Completed state = state.prev.score >= 21 || state.next.score >= 21
+part2Completed state = state.prev.score >= completionScore || state.next.score >= completionScore
 
 part2NotCompleted :: GameState -> Boolean
-part2NotCompleted state = state.prev.score < 21 && state.next.score < 21
+part2NotCompleted state = not (part2Completed state)
 
 type Result = { wins1 :: Int, wins2 :: Int }
 
 resultSum :: Result -> Result -> Result
 resultSum a b = { wins1: a.wins1 + b.wins1, wins2: a.wins2 + b.wins2 }
 
+rollResults :: Array Int
+rollResults = [ 3, 4, 5, 4, 5, 6, 5, 6, 7, 4, 5, 6, 5, 6, 7, 6, 7, 8, 5, 6, 7, 6, 7, 8, 7, 8, 9 ]
+
 iterate :: GameState -> Result
 iterate state = do
-  let nextStates = map (\aggregate -> next2 aggregate state) aggregates
+  let nextStates = map (\result -> next2 result state) rollResults
   let complete = filter part2Completed nextStates
 
   let wins = {
-    wins1: sum $ (map (\st -> st.next.wins)) complete,
-    wins2: sum $ (map (\st -> st.prev.wins)) complete
+    wins1: (sum $ (map (\st -> st.multiplier * st.next.wins)) complete),
+    wins2: (sum $ (map (\st -> st.multiplier * st.prev.wins)) complete)
   }
 
-  let incomplete = filter part2NotCompleted nextStates
-  let iterated = map (\st -> iterate st) incomplete
+  let iterated = map (\st -> iterate st) $ filter part2NotCompleted nextStates
 
   let nestedWins = {
     wins1: sum (map (\result -> result.wins2) iterated),
@@ -162,10 +109,10 @@ iterate state = do
   --trace (show result) \_ -> result
   result
 
---
+-- setup
 
 initialState :: GameState
-initialState =  { die: 100, rollCount: 0,
+initialState =  { die: 100, rollCount: 0, multiplier: 1,
                   next: { position: 7, score: 0, wins: 0 },
                   prev: { position: 1, score: 0, wins: 0 }
                 }
@@ -173,9 +120,6 @@ initialState =  { die: 100, rollCount: 0,
 main :: Effect Unit
 main = do
   log $ "Part 1: " <> (show $ part1Result $ final initialState part1Completed)
-  part2
 
-part2 :: Effect Unit
-part2 = do
-  log $ "Part 2: " <> (show $ iterate initialState)
-
+  let result2 = iterate initialState
+  log $ "Part 2: " <> (show $ (max result2.wins1 result2.wins2))
---
+++ 2021/21/spago.dhall
@@ -0,0 +1,16 @@
+{ name = "AoC-2021-12-20"
+, dependencies =
+  [ "arrays"
+  , "console"
+  , "debug"
+  , "effect"
+  , "foldable-traversable"
+  , "integers"
+  , "math"
+  , "maybe"
+  , "prelude"
+  , "psci-support"
+  ]
+, packages = ./packages.dhall
+, sources = [ "**/*.purs" ]
+}
