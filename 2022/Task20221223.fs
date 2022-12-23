09ed5972b8afae3d4a6d72903524f4f479fdba56
Matias Ärje <matias@arje.net>
2022-12-23 22:11:21 +0200

    Some updates

--- 2022/Task20221223.fs
+++ 2022/Task20221223.fs
@@ -25,7 +25,7 @@ type Task20221223 () =
                     [ (1, 0); (1, -1); (1, 1) ]
                 ]
 
-            let allNeighbors = allLookups |> List.concat |> Set.ofList
+            let allNeighbors = allLookups |> List.concat |> List.distinct
 
             let getBounds state = (
                     state |> Set.map fst |> Set.minElement, state |> Set.map snd |> Set.minElement,
@@ -36,38 +36,39 @@ type Task20221223 () =
                 let shift = allLookups |> List.take (step % 4)
                 let currentLookups = (allLookups |> List.skip (step % 4)) @ shift
 
-                let hasNeighbor (cx, cy) neighbors = neighbors |> Seq.tryFind(fun (dx, dy) -> state |> Set.contains (cx + dx, cy + dy)) <> None
+                let hasNeighbor (cx, cy) neighbors = neighbors |> List.tryFind(fun (dx, dy) -> state |> Set.contains (cx + dx, cy + dy)) <> None
 
-                let rec nextPosition (x, y) lookups =
-                    match hasNeighbor (x, y) allNeighbors, lookups with
-                    | false, _ -> ((x, y), (x, y))
-                    | true, l::_ when not(hasNeighbor (x, y) l) ->
-                        let (mx, my) = l |> List.head
-                        ((x, y), (x + mx, y + my))
-                    | true, _::t -> nextPosition (x, y) t
-                    | _, _ -> ((x, y), (x, y))
+                let rec findDirection (x, y) = function
+                    | l::t when hasNeighbor (x, y) l -> findDirection (x, y) t
+                    | ((mx, my) :: _) :: _ -> ((x, y), (x + mx, y + my))
+                    | _ -> ((x, y), (x, y))
 
-                let getConflictingTargets (positions:(int*int) list) =
-                    positions |> List.countBy id |> List.filter(fun (_, c) -> c >= 2) |> List.map fst |> Set.ofList
+                let nextPosition pos =
+                    match hasNeighbor pos allNeighbors with
+                    | false -> (pos, pos)
+                    | true -> findDirection pos currentLookups
 
-                let nextPositions =
-                    state |> Set.toList |> List.map (fun p -> nextPosition p currentLookups)
+                let nextPositions = state |> Set.map nextPosition
 
-                let conflicts = getConflictingTargets (nextPositions |> List.map snd)
+                let conflicts =
+                    nextPositions
+                    |> Set.toList
+                    |> List.countBy snd
+                    |> List.filter(fun (_, c) -> c >= 2)
+                    |> List.map fst
+                    |> Set.ofList
 
                 let nextState =
                     nextPositions
-                    |> List.map (fun (fromPosition, toPosition) ->
+                    |> Set.map (fun (fromPosition, toPosition) ->
                         match conflicts |> Set.contains toPosition with
                         | true -> fromPosition
                         | false -> toPosition
                     )
-                    |> Set.ofList
 
                 (state <> nextState, nextState)
 
             let finalState = { 0..9 } |> Seq.fold(fun state round -> move round state |> snd) initialState
-
             let minX, minY, maxX, maxY = getBounds finalState
             let part1 = (maxX - minX + 1) * (maxY - minY + 1) - (finalState |> Set.count)
 
@@ -78,8 +79,7 @@ type Task20221223 () =
                     | (false, _) -> None
                     | (true, s) -> Some (round, (s, round + 1))
                 )
-                |> Seq.rev
-                |> Seq.head
+                |> Seq.max
                 |> (+) 2
 
             (part1, part2)
