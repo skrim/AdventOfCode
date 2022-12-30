44b2ae05655068acec3a016c0f9a451681e34ee9
Matias Ärje <matias@arje.net>
2022-12-30 08:10:39 +0200

    Better output, profiler connect helper

--- 2022/Program.fs
+++ 2022/Program.fs
@@ -13,14 +13,19 @@ type Program () =
 
     [<EntryPoint>]
     static let main args =
+
+        if args |> Array.length >= 2 && args[1] = "p" then
+            printfn "Process: %d" (Process.GetCurrentProcess().Id)
+            ignore <| Console.ReadLine()
+
         let writeColor text column =
-            Console.ForegroundColor <- [| ConsoleColor.White; ConsoleColor.Green; ConsoleColor.Green; ConsoleColor.Cyan |][column]
+            Console.ForegroundColor <- [| ConsoleColor.White; ConsoleColor.Green; ConsoleColor.Green; ConsoleColor.Cyan; ConsoleColor.Cyan |][column]
             printf "%s" text
             Console.ResetColor()
 
         let argFilter =
             match args |> Array.length with
-            | 1 -> (fun (t:Type) -> t.Name.Contains(args[0]))
+            | 1 | 2 -> (fun (t:Type) -> t.Name.Contains(args[0]))
             | _ -> fun _ -> true
 
         let types =
@@ -34,7 +39,6 @@ type Program () =
         let results =
             getInstances types
             |> List.mapi (fun i x ->
-                printfn "Solved %d / %d tasks..." i (List.length types)
                 let taskType = x.GetType()
                 let taskName = taskType.Name
                 let fileName = Path.Join("data", sprintf "%s.txt" taskName)
@@ -57,20 +61,24 @@ type Program () =
                     else
                         raise <| new InvalidOperationException(taskType.Name + " has unknown type parameters in its interface")
                 sw.Stop()
+                printf "\r"
+                printf "Solved %d / %d tasks..." (i + 1) (List.length types)
                 (taskName :: result @ [ (sw.ElapsedMilliseconds |> string) + " ms" ], sw.ElapsedMilliseconds)
             )
 
-        let totalTime = results |> List.map snd |> List.sum
+        printfn ""
+
+        let totalTime = results |> List.map snd |> List.sum |> float32
 
         let answers =
-            [([ "Total"; ""; ""; (totalTime |> string) + " ms" ], 0L)]
+            [([ "Total"; ""; ""; (totalTime |> string) + " ms"; "" ], 0L)]
             |> List.append results
             |> List.map (fun a ->
-                a |> fst |> List.map (fun v -> v.Split("\n") |> Array.toList)
+                (a |> fst |> List.map (fun v -> v.Split("\n") |> Array.toList)) @ [ [ sprintf "%.2f %%" ((a |> snd |> float32) * 100.0f / totalTime) ] ]
             )
 
         let columnSizes =
-            [ 0..3 ]
+            [ 0..4 ]
             |> List.map(fun index ->
                 answers
                 |> List.map(fun row ->
@@ -84,7 +92,7 @@ type Program () =
         for row in answers do
             let lines = row |> List.map List.length |> List.max
             for line in { 1..lines } do
-                for column in { 0..3 } do
+                for column in { 0..4 } do
                     let size = columnSizes[column]
                     let value = row[column]
                     if List.length value < line then
