0e7f425fd50c96d52deca7e7868fcbf2a095d51c
Matias Ärje <matias@arje.net>
2023-12-27 19:39:44 +0200

    Add days 10 and 11

--- 2023/src/day10.zig
+++ 2023/src/day10.zig
@@ -1,46 +1,181 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
 const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day10.txt");
 
-pub fn main() !void {
-    
+const print = std.debug.print;
+
+const NotAllowed: isize = 0;
+const Up: isize = 1;
+const Down: isize = 2;
+const Left: isize = 3;
+const Right: isize = 4;
+
+var height: isize = 0;
+var width: isize = 0;
+var area: List(Node) = List(Node).init(gpa);
+
+const Node: type = struct {
+    shape: u8,
+    inLoop: bool = false,
+
+    pub fn setInLoop(self: Node) void {
+        self.inLoop = true;
+    }
+
+    pub fn findExit(self: Node, entry: isize) isize {
+        const shapes = [_]u8 { '|', '|', '-', '-', 'L', 'L', 'J', 'J', '7', '7', 'F', 'F' };
+        const entryDirs = [_]isize { Up, Down, Left, Right, Down, Left, Down, Right, Up, Right, Up, Left };
+        const resultDirs = [_]isize { Up, Down, Left, Right, Right, Up, Left, Up, Left, Down, Right, Down };
+
+        for (shapes, entryDirs, resultDirs) |shape, entryDir, resultDir| {
+            if (self.shape == shape and entry == entryDir)
+                return resultDir;
+        }
+        return NotAllowed;
+    }
+
+    pub fn init(shape: u8) Node {
+        return Node { .shape = shape };
+    }
+
+    pub fn determineDirection(dir1: isize, dir2: isize) u8 {
+        if (dir1 == Down and dir2 == Up) return '|';
+        if (dir1 == Down and dir2 == Right) return 'F';
+        if (dir1 == Down and dir2 == Left) return '7';
+        if (dir2 == Up and dir1 == Right) return 'L';
+        if (dir2 == Up and dir1 == Left) return 'J';
+        if (dir1 == Right and dir2 == Left) return '-';
+        unreachable;
+    }
+};
+
+var externalNode = Node.init('.');
+
+const Crawler: type = struct {
+    lastDirection: isize,
+    x: isize,
+    y: isize,
+    moves: isize,
+
+    pub fn move(self: *Crawler) void {
+        var node = getNode(self.x, self.y);
+        node.inLoop = true;
+        const direction = node.findExit(self.lastDirection);
+
+        switch (direction) {
+            Up => self.y -= 1,
+            Down => self.y += 1,
+            Left => self.x -= 1,
+            Right => self.x += 1,
+            else => unreachable
+        }
+        self.lastDirection = direction;
+        self.moves += 1;
+    }
+};
+
+pub fn getNode(x: isize, y: isize) *Node {
+    if (x < 0 or y < 0 or x >= width or y >= height)
+        return &externalNode;
+
+    const ux = @as(usize, @intCast(x));
+    const uy = @as(usize, @intCast(y));
+    const uw = @as(usize, @intCast(width));
+
+    return &area.items[uy * uw + ux];
 }
 
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
+pub fn initCrawler(x: isize, y: isize, counter: isize) Crawler {
+    const dxs = [_]isize { 0, 0, 1, -1 };
+    const dys = [_]isize { 1, -1, 0, 0 };
+    const dirs = [_]isize { Down, Up, Right, Left };
 
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
+    var c : isize = counter;
+    for (dxs, dys, dirs) |dx, dy, dir| {
+        var node = getNode(x + dx, y + dy);
+        if (node.findExit(dir) != NotAllowed) {
+            c -= 1;
+            if (c == 0)
+                return Crawler { .lastDirection = dir, .x = x + dx, .y = y + dy, .moves = 1 };
+        }
+    }
+    unreachable;
+}
 
-const print = std.debug.print;
-const assert = std.debug.assert;
+fn countInsideNodes() isize {
+    var result : isize = 0;
+    var y: isize = 0;
+    while (y < height) {
+        var inside: bool = false;
+        var entry: u8 = ' ';
 
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
+        var x: isize = 0;
+        while (x < width) {
+            const node = getNode(x, y);
+            const shape: u8 = node.shape;
 
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
+            if (node.inLoop) {
+                if (shape == '|') {
+                    inside = !inside;
+                } else if (shape != '-') {
+                    if (shape == 'L' or shape == 'F')
+                        entry = shape;
+
+                    if (entry == 'L' and shape == '7') inside = !inside;
+                    if (entry == 'F' and shape == 'J') inside = !inside;
+                }
+            }
+            if (!node.inLoop and inside)
+                result += 1;
+
+            x += 1;
+        }
+        y += 1;
+    }
+    return result;
+}
+
+pub fn main() !void {
+    var startX: isize = 0;
+    var startY: isize = 0;
+
+    var lineIterator = std.mem.tokenize(u8, data, "\r\n");
+    while (lineIterator.next()) |line| {
+        width = 0;
+        for (line) |c| {
+            area.append(Node.init(c)) catch unreachable;
+
+            if (c == 'S') {
+                startX = width;
+                startY = height;
+            }
+            width += 1;
+        }
+        height += 1;
+    }
+
+    var crawler1 = initCrawler(startX, startY, 1);
+    var crawler2 = initCrawler(startX, startY, 2);
+
+    var startNode = getNode(startX, startY);
+    const startDir = Node.determineDirection(crawler1.lastDirection, crawler2.lastDirection);
+    startNode.shape = startDir;
+    startNode.inLoop = true;
+
+    while (crawler1.x != crawler2.x or crawler1.y != crawler2.y) {
+        crawler1.move();
+        crawler2.move();
+    }
+
+    const part1: isize = (&crawler1).moves;
+    crawler1.move(); // mark last as in loop
+
+    const part2: isize = countInsideNodes();
+
+    print("Part1: {d}\n", .{part1});
+    print("Part2: {d}\n", .{part2});
+}
--- 2023/src/day11.zig
+++ 2023/src/day11.zig
@@ -1,46 +1,63 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
 const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
+
+const print = std.debug.print;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day11.txt");
 
-pub fn main() !void {
-    
+const Galaxy: type = struct {
+    x: usize,
+    y: usize
+};
+
+var galaxies = List(Galaxy).init(gpa);
+var emptyColumns = List(bool).init(gpa);
+var emptyRows = List(bool).init(gpa);
+
+fn calculateDistance(pos1: usize, pos2: usize, emptyItems: []bool, emptyDistance: usize) usize {
+    var distance : usize = 0;
+    for (@min(pos1, pos2) .. @max(pos1, pos2)) |pos| {
+        if (emptyItems[pos])
+            distance += emptyDistance
+        else
+            distance += 1;
+    }
+    return distance;
 }
 
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
+fn calculateDistances(emptyDistance: usize) usize {
+    var distance: usize = 0;
 
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
+    for (galaxies.items[0 .. galaxies.items.len - 1], 0 .. galaxies.items.len - 1) |g1, index| {
+        for (galaxies.items[index + 1 .. galaxies.items.len]) |g2| {
+            distance += calculateDistance(g1.x, g2.x, emptyColumns.items, emptyDistance);
+            distance += calculateDistance(g1.y, g2.y, emptyRows.items, emptyDistance);
+        }
+    }
+    return distance;
+}
 
-const print = std.debug.print;
-const assert = std.debug.assert;
+pub fn main() !void {
+    var height: usize = 0;
+    var lineIterator = std.mem.tokenize(u8, data, "\r\n");
+    while (lineIterator.next()) |line| : (height += 1) {
+        emptyRows.append(true) catch unreachable;
 
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
+        for (line, 0..) |c, w| {
+            if (height == 0)
+                emptyColumns.append(true) catch unreachable;
 
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
+            if (c == '#') {
+                galaxies.append(Galaxy{.x = w, .y = height}) catch unreachable;
+                emptyColumns.items[w] = false;
+                emptyRows.items[height] = false;
+            }
+        }
+    }
+
+    print("Part 1: {}\n", .{calculateDistances(1)});
+    print("Part 2: {}\n", .{calculateDistances(1000000)});
+}
